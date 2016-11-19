b9a918a6c2c0f52679a494e6519ad386f7badacd
Kelly ONeil <kelly.brianne.oneil@gmail.com>
2016-11-19 12:54:05 -0600

    day 6 get index.php showing, try to connect with db

---
+++ public/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Sugi Keith Photography</h1>
+
+<?php
+echo "Welcome";
+?>
+
+</body>
+<?php
+
+   $host        = "host=localhost";
+   $port        = "port=8000";
+   $dbname      = "dbname=slogans";
+
+   $db = pg_connect( "$host $port $dbname" );
+   if(!$db){
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
+   echo "connected";
+?>
+</html>
